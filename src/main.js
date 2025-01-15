1c5ddcc56ca5e860dbf139461dcc775e38450cf5
evmarino <evmarino@ucsc.edu>
2025-01-15 09:32:00 -0800

    Class walkthrough: Making a Scene

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// Code Practice: Making a Scene
+// Name: Evelyn Marino
+// Date: January 13, 2025
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    scene: [MainMenu, Play ],
+}
+
+let game = new Phaser.Game(config)
